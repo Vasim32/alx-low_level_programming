30ca811b2909b17d54660eb58ecc75692d2ae565
Basim Nuru <Basimnuru@gmail.com>
2022-07-25 08:35:50 -0700

    find me

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-* main -> this is a funtion that printsthe name of the file
+* main - > this is a funtion that printsthe name of the file
 * @argc : argc parameter
 * @argv : an array of a command listed
 * Return : 0 for a success
 */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-printf("%s", *argv[]);
+printf("%s", *argv);
 return (0);
 }
