ef370bc476a0b1cff176a9f2ff883a5f0b000309
Basim Nuru <Basimnuru@gmail.com>
2022-07-25 07:19:46 -0700

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+* main -> this is a funtion that printsthe name of the file
+* @argc : argc parameter
+* @argv : an array of a command listed
+* Return : 0 for a success
+*/
+
 int main(int argc, char *argv[])
 {
-  printf("%s", argv[0]);
+printf("%s", argv[0]);
 return (0);
 }
