d98a4a6edf26401000d3803d33270f1ddd32c815
Vasim32 <Basimnuru@gmail.com>
2022-08-01 21:04:20 -0700

    owow

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef STRTYP
+#define STRTYP
+
+/**
+
+ * struct dog -> strucutre of a dog
+ *@name: name of a dog.
+ *@age: age of a dog.
+ *@owner: owner of a dog.
+ */
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
