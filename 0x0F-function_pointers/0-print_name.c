496df5d8157e36efd77e6b40525b672037552b68
Vasim32 <Basimnuru@gmail.com>
2022-08-07 21:22:09 -0700

    when i see u again

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+/**
+  *print_name - prints a name.
+  *@name: pointer to name.
+  *@f: function pointer.
+/**
+  *print_name - prints a name.
+  *@name: pointer to name.
+  *@f: function pointer.
+  *
+  *Return: void.
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
