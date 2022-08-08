4480a83c86ba2f23669422d5b5f21b2dc43f5f2b
Vasim32 <Basimnuru@gmail.com>
2022-08-08 12:33:00 -0700

    hui

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <stdlib.h>
 
 /**
  * get_op_func - Selects the correct function to perform
@@ -25,4 +24,4 @@ int (*get_op_func(char *s))(int, int)
 		i++;
 
 	return (ops[i].f);
-}`
+}
